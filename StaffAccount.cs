809bc192f1a379a3ff9875bbc4dfa96dbfc4f55e
https://github.com/Samson-Dada <samsondada09@gmail.com>
2022-11-28 13:47:53 +0100

    Inital commit

---
+++ StaffAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaffRecord
+{
+    internal class StaffAccount
+    {
+
+
+
+        //firstname, lastname, email, password
+
+        public void Department(List<string> account)
+        {
+
+        }
+       public void Register(string firstName, string lastName, string password)
+        {
+
+        }
+        public void Login(string firstName, string lastName, string password)
+        {
+
+        }
+        public void Register(string firstName, string lastName, string password)
+        {
+
+        }
+
+        public void EditAccount()
+        {
+
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+    }
+}
